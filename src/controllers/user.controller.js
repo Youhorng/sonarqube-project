da26f9e21381ca44e6ac036cea329e85098fb59b
Youhorng <2023221kean@aupp.edu.kh>
2026-03-23 15:25:28 +0700

    fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,7 +44,43 @@ const updateUser = async (req, res, next) => {
   }
 };
 
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await userService.getUsers();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const user = await userService.getUserById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await userService.deleteUser(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   createUser,
-  updateUser
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
 };
